3744580c44531abae1199c6ee42bb96adac4e5dd
Juandinet <83679618+juandinetUPC@users.noreply.github.com>
2022-07-04 22:57:39 -0500

    valida PDFs - numero de cuenta variable

--- constancias-voae.py
+++ constancias-voae.py
@@ -3,6 +3,7 @@ import os
 from tkinter import filedialog
 import PyPDF2
 
+
 root.directory = filedialog.askdirectory()
 
 route = root.directory+'/'
@@ -10,17 +11,22 @@ route = root.directory+'/'
 constancias = os.listdir(route)
 
 for constancia in constancias:
-    pdfFileObject   = open(route+constancia,'rb')
-    pdfReader       = PyPDF2.PdfFileReader(pdfFileObject)
-    pageObject      = pdfReader.getPage(0)
-    text            = pageObject.extractText()
-    textLength      = len(text)
-    accountNumber   = ""
-
-    for letter in range(textLength):
-        if text[letter]=="C" and text[letter+1]=="t" and text[letter+2]=="a" and text[letter+3]==".":
-            accountNumber = text[letter+4:letter+18]
-            accountNumber = accountNumber.strip()
-            pdfFileObject.close()
-
-            os.rename(route+constancia,route+accountNumber+'.pdf')
+    if constancia[-3:] == 'pdf': # verifica Si el archivo es un pdf por si hay mas archivos o carpetas que no sean pdf
+        pdfFileObject   = open(route+constancia,'rb')
+        pdfReader       = PyPDF2.PdfFileReader(pdfFileObject)
+        pageObject      = pdfReader.getPage(0)
+        text            = pageObject.extractText()
+        textLength      = len(text)
+        
+        for letter in range(textLength):
+            accountNumber   = "" # reinicia account number
+            if text[letter]=="C" and text[letter+1]=="t" and text[letter+2]=="a" and text[letter+3]==".":
+                for i in range(letter+4,letter+20):# recorre los siguientes 20 caracteres
+                    if text[i]==",": # si encuentra una coma, termina el account number
+                        break
+                    else:
+                        accountNumber += text[i] # si no, agrega el caracter al account number
+                accountNumber = accountNumber.strip()# quita los espacios al inicio y al final
+                pdfFileObject.close()
+                os.rename(route+constancia,route+accountNumber+'.pdf') # renombra el archivo
+                break # termina el for para que no se siga buscando
